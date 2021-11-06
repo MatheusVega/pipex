dfd600989f2c76fc3338bb21f8eeaad00320bff7
MatheusVega <matheus.jveiga@gmail.com>
2021-11-05 22:29:40 -0300

    finish

--- main_bonus.c
+++ main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mathfern <mathfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 20:00:06 by mathfern          #+#    #+#             */
-/*   Updated: 2021/11/05 22:11:03 by mathfern         ###   ########.fr       */
+/*   Updated: 2021/11/05 22:28:06 by mathfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,14 @@ void	ft_exit(int code, char *str)
 	}
 }
 
-static void	loop(int numbloop, int fdin, char	*path,  char **envp, char **argv)
+static void	loop(int numbloop, int fdin, char **envp, char **argv)
 {
 	int		i;
 	char	**cmd;
+	char	*path;
 
 	i = 3;
+	path = ft_getPath(envp);
 	while (i < numbloop - 2)
 	{
 		cmd = ft_split(argv[i++], ' ');
@@ -52,12 +54,13 @@ int	main(int argc, char **argv, char **envp)
 	cmd2 = ft_split(argv[argc - 2], ' ');
 	if (!cmd1)
 		ft_exit(-1, "cmd");
-	ft_exit(fdout = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 777), "outfile error");
+	ft_exit(fdout = open(argv[argc - 1], O_RDWR | O_CREAT | O_TRUNC, 777),
+		"outfile error");
 	ft_exit(fdin = open(argv[1], O_RDONLY), "Erro no infile");
 	dup2(fdin, STDIN_FILENO);
 	dup2(fdout, STDOUT_FILENO);
 	ft_pipex(fdin, path, envp, cmd1);
-	loop(argc, fdin, path, envp, argv);
+	loop(argc, fdin, envp, argv);
 	ft_execve(path, cmd2, envp);
 	ft_freecmd(&cmd2);
 	return (1);
